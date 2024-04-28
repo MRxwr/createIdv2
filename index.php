756708d00cf69edf5750d20c25925c4496b0b69c
MRxwr <nasserhatab@gmail.com>
2024-04-29 02:00:48 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,55 +1,157 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Electronic Card Web App</title>
-    <link rel="stylesheet" href="styles.css"> <!-- Link your CSS file here for styling -->
+<html lang="en" >
+
+<head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+    
+    <title>OrderMerch Pre-Lander Template</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <style>
+        body {
+	width: 100wh;
+	height: 90vh;
+	color: #fff;
+	background: linear-gradient(-45deg, #EE7752, #E73C7E, #23A6D5, #23D5AB);
+	background-size: 400% 400%;
+	-webkit-animation: Gradient 15s ease infinite;
+	-moz-animation: Gradient 15s ease infinite;
+	animation: Gradient 15s ease infinite;
+}
+
+@-webkit-keyframes Gradient {
+	0% {
+		background-position: 0% 50%
+	}
+	50% {
+		background-position: 100% 50%
+	}
+	100% {
+		background-position: 0% 50%
+	}
+}
+
+@-moz-keyframes Gradient {
+	0% {
+		background-position: 0% 50%
+	}
+	50% {
+		background-position: 100% 50%
+	}
+	100% {
+		background-position: 0% 50%
+	}
+}
+
+@keyframes Gradient {
+	0% {
+		background-position: 0% 50%
+	}
+	50% {
+		background-position: 100% 50%
+	}
+	100% {
+		background-position: 0% 50%
+	}
+}
+
+h1,
+h6 {
+	font-family: 'Open Sans';
+	font-weight: 300;
+	text-align: center;
+	position: absolute;
+	top: 45%;
+	right: 0;
+	left: 0;
+}
+
+.shake {
+	animation: shake-animation 4.72s ease infinite;
+	transform-origin: 50% 50%;
+  }
+  .element {
+	margin: 0 auto;
+	width: 150px;
+	height: 150px;
+	background: red;
+  }
+  @keyframes shake-animation {
+	 0% { transform:translate(0,0) }
+	1.78571% { transform:translate(5px,0) }
+	3.57143% { transform:translate(0,0) }
+	5.35714% { transform:translate(5px,0) }
+	7.14286% { transform:translate(0,0) }
+	8.92857% { transform:translate(5px,0) }
+	10.71429% { transform:translate(0,0) }
+	100% { transform:translate(0,0) }
+  }
+
+  .backdrop {
+	  -moz-box-shadow: 0px 6px 5px #111; 
+	  -webkit-box-shadow: 0px 6px 5px #111; 
+	  box-shadow: 0px 2px 10px #111; 
+	  -moz-border-radius:190px; 
+	  -webkit-border-radius:190px; 
+	  border-radius:190px;
+  }
+
+  .linktree {
+	  width: 120px;
+	  height: 120px;
+	  background-image: url("https://i.imgur.com/oggcsIH.jpg");
+	  background-size: cover;
+	  background-repeat: no-repeat;
+	  background-position: 50% 50%;
+  }
+  </style>
 </head>
+
 <body>
-    <header>
-        <h1>Welcome to Your Electronic Card</h1>
-        <!-- Include any header elements or navigation links here -->
-    </header>
+        <!-- Alpha.ly Version 0.1 | Development Version -->
+    <div class="container">
+    <div class="col-xs-12">
+            <div class="text-center" style="padding-top: 30px; padding-bottom: 30px;">
+                <img class="backdrop linktree">
+                <h2 style="color: #ffffff; padding-top: 20px;">Instagram Growth Expert</h2>
+            </div>
+    </div>
+    </div>
 
-    <main>
-        <section class="profile">
-            <h2>Your Profile</h2>
-            <!-- Include profile information and links to social media accounts here -->
-        </section>
 
-        <section class="customize">
-            <h2>Customize Your Card</h2>
-            <!-- Include customization options here, such as background color and social media button colors -->
-        </section>
+    <div class="container">
+    <div class="col-xs-12">
+            <div class="text-center">
+                <div style="padding-bottom: 30px;">
+                    <button onclick="location.href='http://bit.ly/2IZURI7'" type="button" class="btn btn-outline-light shake" style="width: 80%; padding-top:10px; padding-bottom:10px; font-weight: 800;">15% OFF Instagram Growth</button>
+                </div>
+                <div style="padding-bottom: 30px;">
+                    <button onclick="location.href='http://bit.ly/2SVZXES'" type="button" class="btn btn-outline-light" style="width: 80%; padding-top:10px; padding-bottom:10px; font-weight: 600;">Guide: Increasing Your Engagement</button>
+                </div>
+                <div style="padding-bottom: 30px;">
+                    <button onclick="location.href='#'" type="button" class="btn btn-outline-light" style="width: 80%; padding-top:10px; padding-bottom:10px; font-weight: 600;">View My YouTube Channel</button>
+                </div>
+                <div style="padding-bottom: 30px;">
+                    <button onclick="location.href='#'" type="button" class="btn btn-outline-light" style="width: 80%; padding-top:10px; padding-bottom:10px; font-weight: 600;">Connect On LinkedIn</button>
+                </div>
+                <div style="padding-bottom: 30px;">
+                    <button onclick="location.href='#'" type="button" class="btn btn-outline-light" style="width: 80%; padding-top:10px; padding-bottom:10px; font-weight: 600;">My Personal Website</button>
+                </div>
+            </div>
+    </div>
+    </div>
 
-        <section class="share">
-            <h2>Share Your Card</h2>
-            <!-- Include sharing options here, such as generating a unique URL or QR code -->
-        </section>
+        <div class="text-center">
+            <a href="https://www.ordermerch.net/" style="color: #34312f;" target="_blank">powered by OrderMerch</a>
+        </div>
 
-        <section class="privacy">
-            <h2>Privacy Settings</h2>
-            <!-- Include privacy settings options here -->
-        </section>
 
-        <section class="contact">
-            <h2>Contact Form</h2>
-            <form action="contact.php" method="POST">
-                <label for="name">Your Name:</label>
-                <input type="text" id="name" name="name" required>
-                <label for="email">Your Email:</label>
-                <input type="email" id="email" name="email" required>
-                <label for="message">Message:</label>
-                <textarea id="message" name="message" required></textarea>
-                <button type="submit">Submit</button>
-            </form>
-        </section>
-    </main>
-
-    <footer>
-        <p>&copy; 2024 Your Electronic Card</p>
-        <!-- Include any footer elements or links here -->
-    </footer>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <!-- This pre-lander was created by Brandon Nilsson -->
+    <!-- You're welcome to edit, reproduce and change this template as long as original contributation stays present on the website at all times.  -->
 </body>
+
 </html>
