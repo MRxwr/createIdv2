f6d322ca0ccf91a1c9beda5ab12cc0a5aba5ab79
MRxwr <nasserhatab@gmail.com>
2024-04-29 06:02:24 +0300

    Update bladeListOfusers.php

--- admin/views/bladeListOfusers.php
+++ admin/views/bladeListOfusers.php
@@ -1,19 +1,19 @@
 <?php 
 if( isset($_GET["hide"]) && !empty($_GET["hide"]) ){
 	if( updateDB('users',array('hidden'=> '2'),"`id` = '{$_GET["hide"]}'") ){
-		header("LOCATION: ?v=ListOfusers");
+		header("LOCATION: ?v=ListOfUsers");
 	}
 }
 
 if( isset($_GET["show"]) && !empty($_GET["show"]) ){
 	if( updateDB('users',array('hidden'=> '0'),"`id` = '{$_GET["show"]}'") ){
-		header("LOCATION: ?v=ListOfusers");
+		header("LOCATION: ?v=ListOfUsers");
 	}
 }
 
 if( isset($_GET["delId"]) && !empty($_GET["delId"]) ){
 	if( updateDB('users',array('status'=> '1'),"`id` = '{$_GET["delId"]}'") ){
-		header("LOCATION: ?v=ListOfusers");
+		header("LOCATION: ?v=ListOfUsers");
 	}
 }
 
@@ -23,7 +23,7 @@ if( isset($_POST["fullName"]) ){
 	if ( $id == 0 ){
 		$_POST["password"] = sha1($_POST["password"]);
 		if( insertDB("users", $_POST) ){
-			header("LOCATION: ?v=ListOfusers");
+			header("LOCATION: ?v=ListOfUsers");
 		}else{
 		?>
 		<script>
@@ -39,7 +39,7 @@ if( isset($_POST["fullName"]) ){
 			$_POST["password"] = $password[0]["password"];
 		}
 		if( updateDB("users", $_POST, "`id` = '{$id}'") ){
-			header("LOCATION: ?v=ListOfusers");
+			header("LOCATION: ?v=ListOfUsers");
 		}else{
 		?>
 		<script>
@@ -50,12 +50,56 @@ if( isset($_POST["fullName"]) ){
 	}
 }
 ?>
-<div class="row">
+<div class="row">		
 <div class="col-sm-12">
 <div class="panel panel-default card-view">
 <div class="panel-heading">
 <div class="pull-left">
-<h6 class="panel-title txt-dark"><?php echo direction("List of users","قائمة المسجلين") ?></h6>
+	<h6 class="panel-title txt-dark"><?php echo direction("User Details","تفاصيل العضو") ?></h6>
+</div>
+	<div class="clearfix"></div>
+</div>
+<div class="panel-wrapper collapse in">
+<div class="panel-body">
+	<form class="" method="POST" action="" enctype="multipart/form-data">
+		<div class="row m-0">
+			<div class="col-md-6">
+			<label><?php echo direction("Name","الإسم") ?></label>
+			<input type="text" name="fullName" class="form-control" required>
+			</div>
+			
+			<div class="col-md-6">
+			<label><?php echo direction("Email","البريد الإلكتروني") ?></label>
+			<input type="text" name="email" class="form-control" required>
+			</div>
+			
+			<div class="col-md-6">
+			<label><?php echo direction("Password","كلمة المرور") ?></label>
+			<input type="text" name="password" class="form-control" required>
+			</div>
+			
+			<div class="col-md-6">
+			<label><?php echo direction("Mobile","الهاتف") ?></label>
+			<input type="number" min="0" maxlength="8" name="phone" class="form-control" required>
+			</div>
+			
+			<div class="col-md-6" style="margin-top:10px">
+			<input type="submit" class="btn btn-primary" value="<?php echo direction("Submit","أرسل") ?>">
+			<input type="hidden" name="update" value="0">
+			</div>
+		</div>
+	</form>
+</div>
+</div>
+</div>
+</div>
+				
+				<!-- Bordered Table -->
+<div class="col-sm-12">
+<div class="panel panel-default card-view">
+<div class="panel-heading">
+<div class="pull-left">
+<h6 class="panel-title txt-dark"><?php echo direction("List of Users","قائمة الموظفين") ?></h6>
 </div>
 <div class="clearfix"></div>
 </div>
@@ -69,25 +113,42 @@ if( isset($_POST["fullName"]) ){
 		<th><?php echo direction("Name","الإسم") ?></th>
 		<th><?php echo direction("Email","الإيميل") ?></th>
 		<th><?php echo direction("Mobile","الهاتف") ?></th>
-		<th><?php echo direction("Joined","إلتحق") ?></th>
-		<th class="text-nowrap"><?php echo direction("Actions","الخيارات") ?></th>
+		<th class="text-nowrap"><?php echo direction("الخيارات","Actions") ?></th>
 		</tr>
 		</thead>
 		
 		<tbody>
 		<?php 
-		if( $users = selectDB("users","`status` = '0' AND `hidden` = '0'") ){
-			for( $i = 0; $i < sizeof($users); $i++ ){	
+		if( $users = selectDB("users","`status` = '0' AND `hidden` != '1'") ){
+			for( $i = 0; $i < sizeof($users); $i++ ){
+				$counter = $i + 1;
+				if ( $users[$i]["hidden"] == 2 ){
+					$icon = "fa fa-unlock";
+					$link = "?v={$_GET["v"]}&show={$users[$i]["id"]}";
+					$hide = direction("Unlock","فتح الحساب");
+				}else{
+					$icon = "fa fa-lock";
+					$link = "?v={$_GET["v"]}&hide={$users[$i]["id"]}";
+					$hide = direction("Lock","قفل الحساب");
+				}
+				
 				?>
 				<tr>
 				<td id="name<?php echo $users[$i]["id"]?>" ><?php echo $users[$i]["fullName"] ?></td>
 				<td id="email<?php echo $users[$i]["id"]?>" ><?php echo $users[$i]["email"] ?></td>
 				<td id="mobile<?php echo $users[$i]["id"]?>" ><?php echo $users[$i]["phone"] ?></td>
-				<td id="date<?php echo $users[$i]["id"]?>" ><?php echo $users[$i]["date"] ?></td>
 				<td class="text-nowrap">
-					<a href="?v=ClientInfo&id=<?php echo $users[$i]["id"] ?>" class="mr-25" data-toggle="tooltip" data-original-title="<?php echo direction("More","المزيد") ?>"> <i class="fa fa-plus text-inverse m-r-10"></i>
-					<a href="<?php echo "?v={$_GET["v"]}&delId={$users[$i]["id"]}" ?>" data-toggle="tooltip" data-original-title="<?php echo direction("Delete","حذف") ?>"><i class="fa fa-close text-danger"></i>
-				</a>			
+				
+				<a id="<?php echo $users[$i]["id"] ?>" class="mr-25 edit" data-toggle="tooltip" data-original-title="<?php echo direction("Edit","تعديل") ?>"> <i class="fa fa-pencil text-inverse m-r-10"></i>
+				</a>
+				<a href="<?php echo $link ?>" class="mr-25" data-toggle="tooltip" data-original-title="<?php echo $hide ?>"> <i class="<?php echo $icon ?> text-inverse m-r-10"></i>
+				</a>
+				<a href="<?php echo "?v={$_GET["v"]}&delId={$users[$i]["id"]}" ?>" data-toggle="tooltip" data-original-title="<?php echo direction("Delete","حذف") ?>"><i class="fa fa-close text-danger"></i>
+				</a>
+				<div style="display:none">
+					<label id="type<?php echo $users[$i]["id"]?>"><?php echo $users[$i]["empType"] ?></label>
+					<label id="shop<?php echo $users[$i]["id"]?>"><?php echo $users[$i]["shopId"] ?></label>
+				</div>				
 				</td>
 				</tr>
 				<?php
@@ -110,11 +171,11 @@ if( isset($_POST["fullName"]) ){
 		var email = $("#email"+id).html();
 		var name = $("#name"+id).html();
 		var mobile = $("#mobile"+id).html();
-		var dateJoined = $("#date"+id).html();
+		$("input[name=password]").prop("required",false);
 		$("input[name=email]").val(email);
 		$("input[name=phone]").val(mobile);
 		$("input[name=update]").val(id);
 		$("input[name=fullName]").val(name);
-		$("input[name=date]").val(dateJoined);
+		$("input[name=fullName]").focus();
 	})
 </script>
