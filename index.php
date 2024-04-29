5fe57c7db9c80b474d9e077adca4c67f8a62b7c6
MRxwr <nasserhatab@gmail.com>
2024-04-29 03:14:31 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://use.fontawesome.com/245c9398b0.js"></script>
     <?php require_once("css/style.php"); ?>
 </head>
 
