4e62df80fd63e15ae1e9806ed1e4bc6e3217bce3
MRxwr <nasserhatab@gmail.com>
2024-04-30 19:24:04 +0300

    Update index.php

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ if( isset($_GET["account"]) && !empty($_GET["account"]) ){
                 <div class="row">
                     <div class="col-6"><img class="backdrop linktree"></div>
                     <div class="col-6" style="align-content: center;"><h2 style="color: #ffffff;"><?php echo $account["fullName"] ?></h2></div>
-                    <div class="col-12" style="align-content: center;"><h4 style="color: #ffffff;"><?php echo $account["details"] ?></h4></div>
+                    <div class="col-12" style="align-content: center;padding-top: 20px;"><h4 style="color: #ffffff;"><?php echo $account["details"] ?></h4></div>
                 </div>
             </div>
         </div>
