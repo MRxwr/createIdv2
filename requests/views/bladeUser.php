ef7598280cf1fb2e299b83bb284c198b1d42c3c0
MRxwr <nasserhatab@gmail.com>
2024-07-05 14:57:59 +0300

    save

---
+++ requests/views/bladeUser.php
@@ -0,0 +1,23 @@
+<?php
+if( isset($_GET["action"]) && !empty($_GET["action"]) ){
+    if( $_GET["action"] == "login" ){
+        if( !isset($_POST["email"]) || empty($_POST["email"]) ){
+        }else{
+            if( $user = selectDBNew("users",[$_POST["email"]],"`email` LIKE ?","") ){
+            }else{
+                echo outputError(array("msg" => "Email Not Found"));
+            }
+        }
+        if( !isset($_POST["password"]) || empty($_POST["password"]) ){
+            echo outputError(array("msg" => "Password Required"));
+        }
+        if( $user = selectDBNew("users",[$_POST["email"],$_POST["password"]],"`email` LIKE ? AND `password` LIKE ?","") ){
+            echo outputData($user[0]);
+        }else{
+            echo outputError(array("msg" => "Wrong Password"));
+        }
+    }
+}else{
+
+}
+?>
