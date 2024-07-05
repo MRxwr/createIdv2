48824d6545fbe1b5c3807149b9c09dee9ae8529e
MRxwr <nasserhatab@gmail.com>
2024-07-05 15:01:04 +0300

    Update bladeUser.php

--- requests/views/bladeUser.php
+++ requests/views/bladeUser.php
@@ -5,19 +5,19 @@ if( isset($_GET["action"]) && !empty($_GET["action"]) ){
         }else{
             if( $user = selectDBNew("users",[$_POST["email"]],"`email` LIKE ?","") ){
             }else{
-                echo outputError(array("msg" => "Email Not Found"));
+                echo outputError(array("msg" => "Email Not Found"));die();
             }
         }
         if( !isset($_POST["password"]) || empty($_POST["password"]) ){
-            echo outputError(array("msg" => "Password Required"));
+            echo outputError(array("msg" => "Password Required"));die();
         }
         if( $user = selectDBNew("users",[$_POST["email"],$_POST["password"]],"`email` LIKE ? AND `password` LIKE ?","") ){
-            echo outputData($user[0]);
+            echo outputData($user[0]);die();
         }else{
-            echo outputError(array("msg" => "Wrong Password"));
+            echo outputError(array("msg" => "Wrong Password"));die();
         }
     }else{
-        echo outputError(array("msg" => "Action Not Found"));
+        echo outputError(array("msg" => "Action Not Found"));die();
     }
 }else{
 
