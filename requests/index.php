485bd3d49af9306651515bb8503f10bb027e3d37
MRxwr <nasserhatab@gmail.com>
2024-07-09 00:24:27 +0300

    save

--- requests/index.php
+++ requests/index.php
@@ -3,18 +3,6 @@ header("Content-Type: application/json; charset=UTF-8");
 require_once("../admin/includes/config.php");
 require_once("../admin/includes/functions.php");
 
-if ( isset(getallheaders()["createidheader"]) ){
-	$headerAPI =  getallheaders()["createidheader"];
-}else{
-	$error = array("msg"=>"Please set headres");
-	echo outputError($error);die();
-}
-
-if ( $headerAPI != "createIdCreate" ){
-	$error = array("msg"=>"headers value is wrong");
-	echo outputError($error);die();
-}
-
 // get viewed page from pages folder \\
 if( isset($_GET["a"]) && searchFile("views","blade{$_GET["a"]}.php") ){
 	require_once("views/".searchFile("views","blade{$_GET["a"]}.php"));
--- requests/views/bladeUser.php
+++ requests/views/bladeUser.php
@@ -1,6 +1,16 @@
 <?php
 if( isset($_GET["action"]) && !empty($_GET["action"]) ){
     if( $_GET["action"] == "Login" ){
+        if ( isset(getallheaders()["createidheader"]) ){
+            $headerAPI =  getallheaders()["createidheader"];
+        }else{
+            $error = array("msg"=>"Please set headres");
+            echo outputError($error);die();
+        }
+        if ( $headerAPI != "createIdCreate" ){
+            $error = array("msg"=>"headers value is wrong");
+            echo outputError($error);die();
+        }
         if( !isset($_POST["email"]) || empty($_POST["email"]) ){
             echo outputError(array("msg" => "Email Required"));die();
         }else{
