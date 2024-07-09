7ddd1d51e9f06a216328a4bade1987dfd2e0ee2e
MRxwr <nasserhatab@gmail.com>
2024-07-10 01:46:42 +0300

    Update bladePayment.php

--- requests/views/bladePayment.php
+++ requests/views/bladePayment.php
@@ -38,14 +38,11 @@ if( isset($_GET["action"]) && !empty($_GET["action"]) ){
         );
         if( $pay = submitUpayment($data) ){
             $insertData["link"] = $pay["data"]["data"]["link"];
-            /*
             if( insertDB("orders",$insertData) ){
                 echo outputData($pay);die();
             }else{
                 echo outputError(array("msg" => "Order Not Inserted"));die();
             }
-                */
-                echo outputData($pay);die();
         }else{
             echo outputError(array("msg" => "Payment Not Submitted"));die();
         }
