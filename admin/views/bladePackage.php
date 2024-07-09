ffbfef4bd31ba198ccc1011b1a58b01bd3c2c7bc
MRxwr <nasserhatab@gmail.com>
2024-07-09 12:15:54 +0300

    Update bladePackage.php

--- admin/views/bladePackage.php
+++ admin/views/bladePackage.php
@@ -75,12 +75,12 @@ if( isset($_POST["title"]) ){
 
             <div class="col-md-6">
 			<label><?php echo direction("English Details","التفاصيل بالعربي") ?></label>
-			<textarea name="enDetails" class="tinymce" required></textarea>
+			<input type="text" name="enDetails" class="tinymce" required>
 			</div>
 
             <div class="col-md-6">
 			<label><?php echo direction("Arabic Details","التفاصيل بالعربي") ?></label>
-			<textarea name="arDetails" class="tinymce" required></textarea>
+			<input type="text" name="arDetails" class="tinymce" required>
 			</div>
 			
 			<div class="col-md-3">
