65cc1a2d2d0ad641ae0353141bfa96b6d7a63acf
MRxwr <nasserhatab@gmail.com>
2024-07-12 17:29:51 +0300

    Update bladeNotification.php

--- requests/views/bladeNotification.php
+++ requests/views/bladeNotification.php
@@ -1,4 +1,4 @@
 <?php 
-$users = transformArray(selectDB2('firebaseToken','users', "`language` = 0 AND `hidden` = '1' AND `status` = '0'"));
-echo outputData($users);die();
+$users = selectDB2('firebaseToken','users', "`language` = 0 AND `hidden` = '1' AND `status` = '0'");
+echo outputData(transformArray($users));die();
 ?>
