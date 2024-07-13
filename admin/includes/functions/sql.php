7d180715479139fdf85012e3bece1c934339d26d
MRxwr <nasserhatab@gmail.com>
2024-07-13 22:35:59 +0300

    Update sql.php

--- admin/includes/functions/sql.php
+++ admin/includes/functions/sql.php
@@ -216,6 +216,7 @@ function updateDB($table, $data, $where) {
     $stmt = $dbconnect->prepare($sql); 
     $values = array_values($data);
     $stmt->bind_param($params, ...$values);
+    var_dump($stmt->execute());die();
     if( isset($_GET["v"]) && !empty($_GET["v"]) ){
         $array = array(
             "userId" => $userID,
@@ -226,7 +227,6 @@ function updateDB($table, $data, $where) {
         );
         LogsHistory($array);
     }
-    var_dump($stmt->execute());
     if ($stmt->execute()) {
         return 1;
     } else {
