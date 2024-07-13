554583011e1d801093aa4ff445215471fd4044e6
MRxwr <nasserhatab@gmail.com>
2024-07-14 00:17:54 +0300

    Update bladeClick.php

--- requests/views/bladeClick.php
+++ requests/views/bladeClick.php
@@ -7,7 +7,7 @@ if( isset($_COOKIE["CID"]) && !empty($_COOKIE["CID"]) && $_COOKIE["CID"] == $_PO
         $account = selectDBNew("users",[$_POST["account"]],"`url` LIKE ?","");
         $dataInsert = array(
             "profileId" => $_POST["profileId"],
-            "userId" => $account[0]["userId"],
+            "userId" => $account[0]["id"],
             "referer" => $_POST["referrer"],
             "remoteAddress" => $_SERVER["REMOTE_ADDR"],
             "userAgent" => $_SERVER["HTTP_USER_AGENT"],
